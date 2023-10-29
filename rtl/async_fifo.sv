95c366383abd1f3b787310f7902962c2f05e4c4b
alexkharl <sasha_xarlamov@mail.ru>
2023-10-30 00:37:21 +0300

    Feat:init by paper's code

---
+++ rtl/async_fifo.sv
@@ -0,0 +1,63 @@
+module async_fifo #(
+  parameter DSIZE = 8,
+  parameter ASIZE = 4
+) (
+  input              rclk,
+  input              wclk,
+  input              rrst_n,
+  input              wrst_n,
+  input  [DSIZE-1:0] wdata,
+  input              winc,
+  input              rinc,
+  output [DSIZE-1:0] rdata,
+  output             wfull,
+  output             rempty
+);
+
+  wire [ASIZE-1:0] waddr, raddr;
+  wire [ASIZE:0]   wptr, rptr, wq2_rptr, rq2_wptr;
+
+  sync_r2w sync_r2w (
+    .wclk     (wclk),
+    .wrst_n   (wrst_n),
+    .wq2_rptr (wq2_rptr),
+    .rptr     (rptr)
+  );
+
+  sync_w2r sync_w2r (
+    .rclk     (rclk),
+    .rrst_n   (rrst_n),
+    .rq2_wptr (rq2_wptr),
+    .wptr     (wptr)
+  );
+
+  fifomem #(DSIZE, ASIZE) fifomem (
+    .wclk   (wclk),
+    .wclken (winc),
+    .rdata  (rdata),
+    .wdata  (wdata),
+    .waddr  (waddr),
+    .raddr  (raddr),
+    .wfull  (wfull)
+  );
+
+  rptr_empty #(ASIZE) rptr_empty (
+    .rclk     (rclk),
+    .rrst_n   (rrst_n),
+    .rempty   (rempty),
+    .raddr    (raddr),
+    .rptr     (rptr),
+    .rq2_wptr (rq2_wptr),
+    .rinc     (rinc)
+  );
+
+  wptr_full #(ASIZE) wptr_full (
+    .wclk     (wclk),
+    .wrst_n   (wrst_n),
+    .wfull    (wfull),
+    .waddr    (waddr),
+    .wptr     (wptr),
+    .wq2_rptr (wq2_rptr),
+    .winc     (winc)
+  );
+endmodule
---
+++ rtl/fifomem.sv
@@ -0,0 +1,35 @@
+module fifomem #(
+  parameter DATASIZE = 8, // Memory data word width
+  parameter ADDRSIZE = 4  // Number of mem address bits
+) (
+  input                 wclk,
+  input                 wclken,
+  input  [DATASIZE-1:0] wdata,
+  input  [ADDRSIZE-1:0] waddr,
+  input                 raddr,
+  input                 wfull,
+  output [DATASIZE-1:0] rdata
+);
+
+`ifdef VENDORRAM
+  // instantiation of a vendor's dual-port RAM
+  vendor_ram mem (
+    .clk      (wclk),
+    .wclken_n (wfull),
+    .wclken   (wclken),
+    .dout     (rdata),
+    .din      (wdata),
+    .waddr    (waddr),
+    .raddr    (raddr)
+  );
+`else
+  // RTL Verilog memory model
+  localparam DEPTH = 1<<ADDRSIZE;
+  reg [DATASIZE-1:0] mem [0:DEPTH-1];
+
+  assign rdata = mem[raddr];
+
+  always @(posedge wclk)
+    if (wclken && !wfull) mem[waddr] <= wdata;
+`endif
+endmodule
