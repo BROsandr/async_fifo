711dff2611e41e582adbff021359ce1bd4c61fe2
alexkharl <sasha_xarlamov@mail.ru>
2023-10-30 12:45:45 +0300

    Fix(tb):Драйвинг в dut через nba

--- dv/tb.sv
+++ dv/tb.sv
@@ -53,7 +53,8 @@ module tb;
   end
 
   // Queue to push data_in
-  reg [DATA_WIDTH-1:0] wdata;
+  reg [DATA_WIDTH-1:0] wdata, tmp_data;
+  logic                tmp_w_en, tmp_r_en;
 
   async_fifo #(
     .DSIZE (DATA_WIDTH),
@@ -75,24 +76,27 @@ module tb;
   always #35ns rclk = ~rclk;
   
   initial begin
-    wclk = 1'b0; wrst_n = 1'b0;
-    w_en = 1'b0;
-    data_in = 0;
+    wclk <= 1'b0;
+    wrst_n <= 1'b0;
+    w_en <= 1'b0;
+    data_in <= 0;
 
-    beh_w_en    = 1'b0;
-    beh_data_in = 0;
+    beh_w_en    <= 1'b0;
+    beh_data_in <= 0;
 
     repeat(10) @(posedge wclk);
-    wrst_n = 1'b1;
+    wrst_n <= 1'b1;
 
     repeat(2) begin
       for (int i=0; i<30; i++) begin
         @(posedge wclk iff !beh_full);
-        beh_w_en = (i%2 == 0)? 1'b1 : 1'b0;
-        w_en = beh_w_en;
+        tmp_w_en = (i%2 == 0)? 1'b1 : 1'b0;
+        beh_w_en <= tmp_w_en;
+        w_en     <= tmp_w_en;
         if (beh_w_en) begin
-          beh_data_in = $urandom;
-          data_in = beh_data_in;
+          tmp_data = $urandom;
+          beh_data_in <= tmp_data;
+          data_in     <= tmp_data;
         end
       end
       #50;
@@ -100,18 +104,20 @@ module tb;
   end
 
   initial begin
-    rclk = 1'b0; rrst_n = 1'b0;
-    r_en = 1'b0;
-    beh_r_en = 1'b0;
+    rclk <= 1'b0;
+    rrst_n <= 1'b0;
+    r_en <= 1'b0;
+    beh_r_en <= 1'b0;
 
     repeat(20) @(posedge rclk);
-    rrst_n = 1'b1;
+    rrst_n <= 1'b1;
 
     repeat(2) begin
       for (int i=0; i<30; i++) begin
         @(posedge rclk iff !beh_empty);
-        beh_r_en = (i%2 == 0)? 1'b1 : 1'b0;
-        r_en = beh_r_en;
+        tmp_r_en = (i%2 == 0)? 1'b1 : 1'b0;
+        beh_r_en <= tmp_r_en;
+        r_en     <= tmp_r_en;
         if (beh_r_en) begin
           wdata = beh_data_out;
           if(data_out !== wdata) $error("Time = %0t: Comparison Failed: expected wr_data = %h, rd_data = %h", $time, wdata, data_out);
